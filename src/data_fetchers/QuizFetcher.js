8296928201cbf9af9a0c91464189a55c0e5a3ed7
Yusuke Horie <9kj3hg73@gmail.com>
2020-09-20 02:08:38 +0900

    API_URL fixed.

--- src/data_fetchers/QuizFetcher.js
+++ src/data_fetchers/QuizFetcher.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://opentdb.com/api.php?amount=10&type=multiple';
+const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
 class QuizFetcher {
   static async fetch() {
     const response = await axios.get(API_URL);
